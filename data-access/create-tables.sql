2a1fd809df3cf53cbd1ee80a53184f4e98a0a567
jhmin99 <mjh012399@gmail.com>
2024-09-10 22:44:29 +0900

    Add data-access

---
+++ data-access/create-tables.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS album;
+CREATE TABLE album (
+  id         INT AUTO_INCREMENT NOT NULL,
+  title      VARCHAR(128) NOT NULL,
+  artist     VARCHAR(255) NOT NULL,
+  price      DECIMAL(5,2) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO album
+  (title, artist, price)
+VALUES
+  ('Blue Train', 'John Coltrane', 56.99),
+  ('Giant Steps', 'John Coltrane', 63.99),
+  ('Jeru', 'Gerry Mulligan', 17.99),
+  ('Sarah Vaughan', 'Sarah Vaughan', 34.98);
+
+
+-- docker cp /path/to/create-tables.sql mysql-container:/create-tables.sql
+-- docker exec -it mysql-container mysql -u root -p
+-- USE recordings;
+-- soruce /create-tables.sql;
